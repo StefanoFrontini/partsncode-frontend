849366f471bec8e64241ca0f60a67e119094cef1
Stefano Frontini <stefanofrontini75@gmail.com>
2020-11-04 18:26:09 +0100

    bug fix dettagli prodotto

--- gridsome.server.js
+++ gridsome.server.js
@@ -5,40 +5,39 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-module.exports = function (api) {
+module.exports = function(api) {
   api.loadSource(({ addCollection }) => {
     // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
-  })
+  });
 
   api.createPages(async ({ graphql, createPage }) => {
-    const { data } = await graphql(`{
-      allStrapiProdotto {
-        edges {
-          node{
-            id
-            codice
-            
+    const { data } = await graphql(`
+      {
+        allStrapiProdotto {
+          edges {
+            node {
+              id
+              codice
+            }
           }
         }
       }
-    }`);
-    const parsedCodice = (codice) => {      
+    `);
+    const parsedCodice = (codice) => {
       const pattern = /[a-zA-Z\d]/g;
       const new_str = codice.match(pattern);
-      const result = new_str.join('');      
-      return result
-    }
-    
-
+      const result = new_str.join("");
+      return result;
+    };
 
     data.allStrapiProdotto.edges.forEach(({ node }) => {
       createPage({
-        path: `/prodotti/prodotto/codice=${parsedCodice(node.codice)}`,
-        component: './src/templates/Prodotto.vue',
+        path: `/spazzole/spazzola/codice=${parsedCodice(node.codice)}`,
+        component: "./src/templates/Prodotto.vue",
         context: {
-          id: node.id
-        }
-      })
-    })
-  })
-}
+          id: node.id,
+        },
+      });
+    });
+  });
+};
--- src/components/ProdottoCard.vue
+++ src/components/ProdottoCard.vue
@@ -1,67 +1,77 @@
-<template> 
-                   
-            <div class="box">
-              <div class="img-container" @click="$router.push({ path: findLink(prodotto.codice) })" >                
-                <g-image v-if="prodotto.immagine_principale" :src="prodotto.immagine_principale.url" :alt="prodotto.codice + ' ' + prodotto.descrizione"/>              
-                <g-image v-else src="~/assets/imagenotavailable.png"></g-image>
-              </div>
-              <hr>
-              <p class="codice" v-highlight="{codice: prodotto.codice, input: userInput}"></p>
-                <div v-for="cat in prodotto.categories" :key="cat.id">
-                   <p class="category">{{ cat.nome }}</p> 
-                </div>
-                <div class="prezzo">{{ prodotto.prezzo }} €</div> 
-                <div class="btn-wrapper">
-                  
-                <button class="btn" @click="$router.push({ path: findLink(prodotto.codice) })">Maggiori Informazioni</button>
-                </div>            
-            </div>
-
+<template>
+  <div class="box">
+    <div
+      class="img-container"
+      @click="$router.push({ path: findLink(prodotto.codice) })"
+    >
+      <g-image
+        v-if="prodotto.immagine_principale"
+        :src="prodotto.immagine_principale.url"
+        :alt="prodotto.codice + ' ' + prodotto.descrizione"
+      />
+      <g-image v-else src="~/assets/imagenotavailable.png"></g-image>
+    </div>
+    <hr />
+    <p
+      class="codice"
+      v-highlight="{ codice: prodotto.codice, input: userInput }"
+    ></p>
+    <div v-for="cat in prodotto.categories" :key="cat.id">
+      <p class="category">{{ cat.nome }}</p>
+    </div>
+    <div class="prezzo">{{ prodotto.prezzo }} €</div>
+    <div class="btn-wrapper">
+      <button
+        class="btn"
+        @click="$router.push({ path: findLink(prodotto.codice) })"
+      >
+        Maggiori Informazioni
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['prodotto', 'userInput'],
-    methods: {    
-    findLink(codice) {      
+  props: ["prodotto", "userInput"],
+  methods: {
+    findLink(codice) {
       const pattern = /[a-zA-Z\d]/g;
       const new_str = codice.match(pattern);
-      const result = new_str.join('');      
-      return `prodotto/codice=${result}`
+      const result = new_str.join("");
+      return `spazzola/codice=${result}`;
     },
-    
   },
-    directives: {
-      highlight(el, binding) {
-        const regex = new RegExp(binding.value.input, 'gi');
-        const high = binding.value.codice.replace(regex, `<span class='hl'>${binding.value.input}</span>`);
-        el.innerHTML = high;
-      }
+  directives: {
+    highlight(el, binding) {
+      const regex = new RegExp(binding.value.input, "gi");
+      const high = binding.value.codice.replace(
+        regex,
+        `<span class='hl'>${binding.value.input}</span>`
+      );
+      el.innerHTML = high;
     },
-    
-}
+  },
+};
 </script>
 
 <style scoped>
 .codice {
   font-weight: bold;
-  text-align: center;  
+  text-align: center;
 }
 
 .category {
   font-size: 0.65rem;
   text-align: center;
   margin-top: -10px;
-   
 }
-
 
 .box {
   width: 250px;
   margin-left: 10px;
   margin-right: 10px;
   margin-top: 40px;
-  
 }
 .img-container {
   width: 250px;
@@ -72,7 +82,6 @@ export default {
   width: 100%;
   height: 100%;
   object-fit: cover;
-  
 }
 
 .btn {
@@ -85,19 +94,16 @@ export default {
 }
 
 .btn-wrapper {
-  
   display: flex;
   justify-content: center;
-  
 }
 
 .fade-enter-active,
 .fade-leave-active {
-	transition: ease opacity 0.3s;
+  transition: ease opacity 0.3s;
 }
 .fade-enter,
 .fade-leave-to {
-	opacity: 0;
+  opacity: 0;
 }
-
 </style>
